f30db799bc62b117aa0934cd66196caa24e5d535
bl00w <flash364@gmail.com>
2018-10-24 02:57:20 +0500

    ...

--- citilink.js
+++ citilink.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Citilink show sale
 // @namespace    http://tampermonkey.net/
-// @version      0.1.1
+// @version      0.1.2
 // @description  try to take over the world!
 // @author       You
 // @match https://www.citilink.ru/*/
@@ -52,9 +52,5 @@
         }
 
         productPriceElements[0].insertAdjacentHTML('afterEnd', '<div class="price price_break"><ins class="num">real ' + textToInsert + ')</ins><ins class="rub"> руб.</ins></div>');
-
-        //console.log(match);
     });
-
-   // alert(price);
 })();
